cc843f286fd310cc09d0a2be0457f8c3814bec0f
Yannis Lang <jopolaz@hotmail.fr>
2021-10-13 14:45:35 +0200

    Initial Commit

---
+++ Crazy8Visualizer/Crazy8VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crazy8VisualizerApp.swift
+//  Crazy8Visualizer
+//
+//  Created by Yannis Lang on 13/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Crazy8VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
